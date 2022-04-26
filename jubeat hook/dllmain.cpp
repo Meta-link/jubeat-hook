a847af04d255c347c6ac9aa255a62327708a6a09
Meta-link <meta-link@hotmail.fr>
2022-04-26 23:52:46 +0200

    Add ini option file, json error parsing, print function

--- jubeat hook/dllmain.cpp
+++ jubeat hook/dllmain.cpp
@@ -4,11 +4,14 @@
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <format>
+#include <chrono>
 #include "jubeathook.h"
 
 #include <openssl/ssl.h>
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <simpleini.h>
 
 using namespace std;
 using json = nlohmann::json;
@@ -23,24 +26,35 @@ Results* results = nullptr;
 
 AdditionalData additionalData;
 int currentSong = 0;
+ofstream scoreFile;
+json ScoreData;
+json FileContentData;
+json KamaiResponse;
 
+bool showDebug;
+int playerID;
+bool exportFile;
+bool exportKamai;
+string statusURL = "";
+string importURL = "";
+string apiKey = "";
 
-string URLbase = "";
-string URLstatus = "";
-string URLimport = "";
-
-string APIKEY = "";
-
-void chartDump()
+void printDebug(auto str)
 {
-    printf("=== SONG %i ===", currentSong);
+    if(showDebug)
+        cout << "[jubeat hook] " << str << endl;
+}
+
+void ChartDump()
+{
+    printf("    === SONG %i ===\n", currentSong);
     printf("Song ID =           %i\n", chart->ChartId);
     printf("Song difficulty =   %s\n", Difficulty[chart->ChartDifficulty]);
     printf("Song level =        %i.%i\n", chart->ChartLevel, chart->ChartDecimal);
     printf("Hard mode =         %s\n\n", hardMode->HardMode ? "yes" : "no");
 }
 
-void ScoreDump(int trackNumber = 0)
+void ScoreDump()
 {
     printf("Score =             %i\n", scores->ScoresData[currentSong].Score);
     printf("Last combo =        %i\n", scores->ScoresData[currentSong].LastCombo);
@@ -57,8 +71,71 @@ void ScoreDump(int trackNumber = 0)
     printf("Result bonus =      %i\n", results->ResultsData[currentSong].Bonus);
     printf("Result clear =      %i\n\n", results->ResultsData[currentSong].Clear);
 
-    additionalData.ScoreTotal = results->ResultsData[currentSong].Score + results->ResultsData[currentSong].Bonus;
     printf("TOTAL SCORE =       %i\n", additionalData.ScoreTotal);
+    printf("RATING =            %s\n", Rating[additionalData.Rating]);
+    printf("CLEAR =             %s\n", ClearType[additionalData.ClearType]);
+    printf("MUSIC RATE =        %f\n\n", additionalData.MusicRate);
+}
+
+void SetupJson()
+{
+    ScoreData = {
+        {
+            "meta",
+            {
+                {"service", "jubeat hook"},
+                {"game", "jubeat"},
+                {"playtype", "Single"}
+            }
+        },
+        {
+            "scores",
+            {
+
+            }
+        }
+    };
+
+    if (exportFile)
+    {
+        FileContentData = ScoreData;
+    }
+}
+
+void AddScoreJson()
+{
+    ScoreData["scores"] = 
+    {
+
+        {
+            {"score", additionalData.ScoreTotal},
+            {"lamp", ClearType[additionalData.ClearType]},
+            {"percent", additionalData.MusicRate},
+            {"matchType", "inGameID"},
+            {"identifier", to_string(chart->ChartId)},
+            {"difficulty", string(hardMode->HardMode ? "HARD " : "") + string(DifficultyTag[chart->ChartDifficulty])},
+            {"timeAchieved", duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count()},
+            {"judgements",
+                {
+                    {"miss", scores->ScoresData[currentSong].MissCount},
+                    {"poor", scores->ScoresData[currentSong].PoorCount},
+                    {"good", scores->ScoresData[currentSong].GoodCount},
+                    {"great", scores->ScoresData[currentSong].GreatCount},
+                    {"perfect", scores->ScoresData[currentSong].PerfectCount},
+                }
+            },
+        }
+    };
+
+    if (exportFile)
+    {
+        FileContentData.push_back(ScoreData["scores"][0]);
+    }
+}
+
+void ProcessScore()
+{
+    additionalData.ScoreTotal = results->ResultsData[currentSong].Score + results->ResultsData[currentSong].Bonus;
 
     additionalData.Rating = 0;
     if (additionalData.ScoreTotal <= 499999)
@@ -77,7 +154,6 @@ void ScoreDump(int trackNumber = 0)
         additionalData.Rating = 2;
     else if (additionalData.ScoreTotal <= 999999)
         additionalData.Rating = 1;
-    printf("RATING =            %s\n", Rating[additionalData.Rating]);
 
     additionalData.ClearType = 0;
     if (additionalData.ScoreTotal >= 700000)
@@ -89,61 +165,50 @@ void ScoreDump(int trackNumber = 0)
         else
             additionalData.ClearType = 1;
     }
-    printf("CLEAR =             %s\n", ClearType[additionalData.ClearType]);
 
     additionalData.MusicRate = ((scores->ScoresData[currentSong].PerfectCount + 0.2 * scores->ScoresData[currentSong].GreatCount + 0.05 * scores->ScoresData[currentSong].GoodCount) / scores->ScoresData[currentSong].NoteCount) * (hardMode->HardMode ? 120 : 100);
-    printf("MUSIC RATE =        %f\n\n", additionalData.MusicRate);
-}
 
-void SendScore()
-{
-    json outData = {
-        {
-            "meta",
-            {
-                {"service", "jubeat hook"},
-                {"game", "jubeat"},
-                {"playtype", "Single"}
-            }
-        },
-        {
-            "scores",
-            {
-                {
-                    {"score", additionalData.ScoreTotal},
-                    {"lamp", ClearType[additionalData.ClearType]},
-                    {"percent", additionalData.MusicRate},
-                    {"matchType", "inGameID"},
-                    {"identifier", to_string(chart->ChartId)},
-                    {"difficulty", string(hardMode->HardMode ? "HARD " : "") + string(DifficultyTag[chart->ChartDifficulty])},
-                    {"timeAchieved", duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count()},
-                    {"judgements",
-                        {
-                            {"miss", scores->ScoresData[currentSong].MissCount},
-                            {"poor", scores->ScoresData[currentSong].PoorCount},
-                            {"good", scores->ScoresData[currentSong].GoodCount},
-                            {"great", scores->ScoresData[currentSong].GreatCount},
-                            {"perfect", scores->ScoresData[currentSong].PerfectCount},
-                        }
-                    },
-                }
-            }   
-        }
-    };
+    if (showDebug)
+    {
+        ChartDump();
+        ScoreDump();
+    }
+
+    AddScoreJson();
+    printDebug(ScoreData.dump(4));
+
+    if (exportFile)
+    {
+        printDebug("Writing scores into file");
+        scoreFile << ScoreData.dump(4) << endl;
+    }
     
-    cout << "[jubeat hook] Sending score to kamaitachi" << endl;
-    cout << outData.dump(4) << endl;
-    cpr::Response r = cpr::Post(cpr::Url{ URLimport},
-                                cpr::Timeout(4000),
-                                cpr::Header{ {"Authorization", "Bearer " + APIKEY}, {"Content-Type", "application/json"} },
-                                cpr::Body{ outData.dump()});
-    cout << "[jubeat hook] Score sent, response code : " << r.status_code << endl;
-    cout << "[jubeat hook] Response text : " << r.text << endl;
-}
+    if (exportKamai)
+    {
+        printDebug("Sending score to kamaitachi");
+        cpr::Response r = cpr::Post(cpr::Url{ importURL },
+            cpr::Timeout(4000),
+            cpr::Header{ {"Authorization", "Bearer " + apiKey}, {"Content-Type", "application/json"} },
+            cpr::Body{ ScoreData.dump() });
+        printDebug("Response text : " + r.text);
 
-void WriteScore()
-{
-    cout << "[jubeat hook] Writing score in file" << endl;
+        if (!r.text.empty())
+        {
+            printDebug("Checking kamaitachi import");
+            KamaiResponse = json::parse(r.text);
+            r = cpr::Get(cpr::Url{ KamaiResponse["body"]["url"] });
+
+            KamaiResponse = json::parse(r.text);
+            printDebug(KamaiResponse["description"]);
+            
+            if (!KamaiResponse["body"]["import"]["errors"].empty())
+            {
+                printDebug("Error :");
+                printDebug(KamaiResponse["body"]["import"]["errors"][0]["message"]);
+            }
+        }
+    }
+    
 }
 
 DWORD WINAPI InitHook(LPVOID dllInstance)
@@ -151,22 +216,53 @@ DWORD WINAPI InitHook(LPVOID dllInstance)
     AllocConsole();
     freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
 
+    CSimpleIniA ini;
+    SI_Error rc = ini.LoadFile("jubeathook.ini");
+    if (rc < 0) 
+    {
+        cout << "Error while loading jubeathook.ini, ending process ..." <<endl;
+        return EXIT_FAILURE;
+    }
+    showDebug = ini.GetBoolValue("general", "showDebug");
+    //playerID = ini.GetValue("")
+    exportFile = ini.GetBoolValue("general", "exportFile");
+    exportKamai = ini.GetBoolValue("kamaitachi", "exportKamai");
+    statusURL = ini.GetValue("kamaitachi", "statusURL");
+    importURL = ini.GetValue("kamaitachi", "importURL");
+    apiKey = ini.GetValue("kamaitachi", "apiKey");
+
+    printDebug("Starting hook");
+
+    SetupJson();
+
+    if (exportFile)
+    {
+        const auto now = chrono::system_clock::now();
+        string fileName = "scores_" + format("{:%d-%m-%Y_%H%M%OS}", now) + ".json";
+        scoreFile.open(fileName);
+        printDebug("Creating score file : " + fileName);
+    }
+
+    if (exportKamai)
+    {
+        cpr::Response rk = cpr::Get(cpr::Url{ statusURL });
+        printDebug("Checking connection to Kamaitachi : " + (string)rk.url);
+        printDebug("Kamaitachi status :");
+        printDebug(rk.status_code);
+    }
+
     jubeatAdress = (std::uintptr_t)GetModuleHandleA("jubeat.dll");
     chart = (ChartData*)(jubeatAdress + ChartAdress);
     hardMode = (HardModeData*)(jubeatAdress + HardModeAdress);
     scores = (Scores*)(jubeatAdress + ScoreAdress);
     results = (Results*)(jubeatAdress + ResultAdress);
 
-    cpr::Response rk = cpr::Get(cpr::Url{ URLbase + URLstatus });
-    cout << "[jubeat hook] Checking connection to Kamaitachi : " << rk.url << endl;
-    cout << "[jubeat hook] Kamaitachi status : " << rk.status_code << endl;
 
     do {
         if(results->ResultsData[currentSong].Clear != 0)
         {
-            chartDump();
-            ScoreDump();
-            SendScore();
+            ProcessScore();
+
             currentSong++;
             if (currentSong > 2)
                 currentSong = 0;
@@ -176,7 +272,9 @@ DWORD WINAPI InitHook(LPVOID dllInstance)
 
     } while (true);
 
-    printf("Detaching jubeat hook");
+    if(exportFile)
+        scoreFile.close();
+    printDebug("Detaching hook");
 
     FreeConsole();
     FreeLibraryAndExitThread((HMODULE)dllInstance, EXIT_SUCCESS);
